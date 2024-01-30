3605411e5db9f4e79abec0d1928227d95d2996e0
zionabebe34 <zion.abebe@post.runi.ac.il>
2024-01-30 19:30:56 +0200

    add the tpken file

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -4,8 +4,9 @@ public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
-		String hashTag = args[0];
+		String hashTag = "iloveabandon";
 		String []dictionary = readDictionary("dictionary.txt");
+		
 		breakHashTag(hashTag, dictionary);
 	}
 
@@ -13,17 +14,27 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
+		String str = ""; 
 
-		// Your code here
-
+		for ( int i = 0; i < 3000; i++) {
+			str = in.readLine(); 
+			dictionary[i] = str; 
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		boolean isExist = false; 
+		for ( int i = 0; i < dictionary.length; i++) {
+			if ( word.equals(dictionary[i])) {
+				isExist = true; 
+			}
+		}
+		return isExist; 
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
@@ -31,10 +42,21 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
-
+  
         for (int i = 1; i <= N; i++) {
-		
+        	
+
+        	if (existInDictionary(hashtag.substring(0,i)  , dictionary) == true) {
+        		System.out.println(hashtag.substring(0,i));
+        		breakHashTag( hashtag.substring(i) , dictionary);
+        	} else { 
+				continue;
+        		
+        	}
+
         }
     }
+	 
+
 
 }
